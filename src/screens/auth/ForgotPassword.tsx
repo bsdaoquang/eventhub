aca226579a4ed6666e9ac1f90b01d442d1f48804
bsdaoquang <bsdaoquang@gmail.com>
2024-01-28 16:36:39 +0700

    change password

--- src/screens/auth/ForgotPassword.tsx
+++ src/screens/auth/ForgotPassword.tsx
@@ -1,5 +1,5 @@
-import {View, Text} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, Alert} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {
   ButtonComponent,
   ContainerComponent,
@@ -10,14 +10,45 @@ import {
 } from '../../components';
 import {ArrowRight, Sms} from 'iconsax-react-native';
 import {appColors} from '../../constants/appColors';
+import {Validate} from '../../utils/validate';
+import {LoadingModal} from '../../modals';
+import authenticationAPI from '../../apis/authApi';
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleCheckEmail = () => {
+    const isValidEmail = Validate.email(email);
+    setIsDisable(!isValidEmail);
+  };
+
+  const handleForgotPassword = async () => {
+    const api = `/forgotPassword`;
+    setIsLoading(true);
+    try {
+      const res: any = await authenticationAPI.HandleAuthentication(
+        api,
+        {email},
+        'post',
+      );
+
+      console.log(res);
+
+      Alert.alert('Send mail', 'We sended a email includes new password!!!');
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(`Can not create new password api forgot password, ${error}`);
+    }
+  };
 
   return (
-    <ContainerComponent back isImageBackground>
+    <ContainerComponent back isImageBackground isScroll>
       <SectionComponent>
         <TextComponent text="Resset Password" title />
+        <SpaceComponent height={12} />
         <TextComponent text="Please enter your email address to request a password reset" />
         <SpaceComponent height={26} />
         <InputComponent
@@ -25,16 +56,20 @@ const ForgotPassword = () => {
           onChange={val => setEmail(val)}
           affix={<Sms size={20} color={appColors.gray} />}
           placeholder="abc@gmail.com"
+          onEnd={handleCheckEmail}
         />
       </SectionComponent>
       <SectionComponent>
         <ButtonComponent
+          onPress={handleForgotPassword}
+          disable={isDisable}
           text="Send"
           type="primary"
           icon={<ArrowRight size={20} color={appColors.white} />}
           iconFlex="right"
         />
       </SectionComponent>
+      <LoadingModal visible={isLoading} />
     </ContainerComponent>
   );
 };
